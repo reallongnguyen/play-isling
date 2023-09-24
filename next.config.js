3f9c0be6e9cf4aec3f410aac94d8e7583edb2523
Long <longnp@btc-studio.io>
2023-09-25 02:17:04 +0700

    fix bug utf-8-validate when build

--- next.config.js
+++ next.config.js
@@ -4,4 +4,11 @@ module.exports = {
   images: {
     domains: ['images.unsplash.com', 'i.ytimg.com'],
   },
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      bufferutil: 'commonjs bufferutil',
+    })
+    return config
+  },
 }
