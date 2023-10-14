b2adccd1fde1560d74108714bf40032b1615c739
Long <longnp@btc-studio.io>
2023-10-15 03:57:42 +0700

    ui: add  mobile responsive

--- app/all-done/page.tsx
+++ app/all-done/page.tsx
@@ -2,24 +2,17 @@
 
 import { Button } from '@/components/atoms/button'
 import HomeHeader from '@/components/templates/headers/HomeHeader'
-import HomeHeaderForGuest from '@/components/templates/headers/HomeHeaderForGuest'
-import useAccount from '@/lib/account/useAccount'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
 
 function Page() {
-  const { userProfile } = useAccount({ mustLogin: false })
   const query = useSearchParams()
   const nextStep = query.get('next') || '/'
 
   return (
     <>
       <header className="fixed h-12 lg:h-14 top-0 left-0 px-2 lg:px-6 w-full bg-primary z-40">
-        {!userProfile ? (
-          <HomeHeaderForGuest />
-        ) : (
-          <HomeHeader userProfile={userProfile} />
-        )}
+        <HomeHeader />
       </header>
       <div className="h-32" />
       <div className="grid grid-cols-2 place-items-center mx-96">
