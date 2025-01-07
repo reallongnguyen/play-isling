a6e2230048d6641a9f734029ec815df1c4c6644a
Long <longnp@btc-studio.io>
2025-01-08 06:03:49 +0900

    [app] force logout

--- lib/account/useAccount.tsx
+++ lib/account/useAccount.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { getUserProfile } from './repo/api'
 import { ErrorResponse, SuccessResponse } from '../common/models/api-response'
-import { getToken } from './repo/token'
+import { getToken, removeToken } from './repo/token'
 import Profile from './models/profile'
 
 export interface UseAccountProps {
@@ -21,53 +21,56 @@ export default function useAccount(orgProps?: UseAccountProps) {
   const [hasToken, setHasToken] = useState(false)
   const [isTokenLoading, setIsTokenLoading] = useState(true)
 
-  // const router = useRouter()
-  // const {
-  //   data: userProfileRes,
-  //   isLoading,
-  //   refetch: refetchProfile,
-  // } = useQuery<SuccessResponse<Profile>, ErrorResponse>({
-  //   queryFn: getUserProfile,
-  //   staleTime: profileCacheTTL, // enable cache
-  //   queryKey: ['fetchUserProfile'],
-  //   enabled: hasToken,
-  //   retry: (fCount, error) => {
-  //     return fCount < 8 && error.code / 100 >= 5
-  //   },
-  //   retryDelay: (failCount) => 100 * 2 ** Math.min(failCount, 9),
-  // })
-  // const hasAuth = Boolean(userProfileRes) || hasToken
+  const router = useRouter()
+  const {
+    data: userProfileRes,
+    isLoading,
+    refetch: refetchProfile,
+  } = useQuery<SuccessResponse<Profile>, ErrorResponse>({
+    queryFn: getUserProfile,
+    staleTime: profileCacheTTL, // enable cache
+    queryKey: ['fetchUserProfile'],
+    enabled: hasToken,
+    retry: (fCount, error) => {
+      return fCount < 8 && error.code / 100 >= 5
+    },
+    retryDelay: (failCount) => 100 * 2 ** Math.min(failCount, 9),
+  })
+  const hasAuth = Boolean(userProfileRes) || hasToken
 
-  // useEffect(() => {
-  //   const token = getToken()
-  //   setIsTokenLoading(false)
+  useEffect(() => {
+    // force logout to use offline mode
+    removeToken()
 
-  //   if (!token && mustLogin) {
-  //     router.push('/signin')
+    const token = getToken()
+    setIsTokenLoading(false)
 
-  //     return
-  //   }
+    if (!token && mustLogin) {
+      router.push('/signin')
 
-  //   if (token) {
-  //     setHasToken(true)
-  //   }
-  // }, [mustLogin, router])
+      return
+    }
 
-  // useEffect(() => {
-  //   if (
-  //     userProfileRes &&
-  //     userProfileRes.data &&
-  //     !userProfileRes.data.firstName
-  //   ) {
-  //     const nextStep = '/all-done'
-  //     router.push(`/me/profile/edit?next=${nextStep}`)
-  //   }
-  // }, [router, userProfileRes])
+    if (token) {
+      setHasToken(true)
+    }
+  }, [mustLogin, router])
+
+  useEffect(() => {
+    if (
+      userProfileRes &&
+      userProfileRes.data &&
+      !userProfileRes.data.firstName
+    ) {
+      const nextStep = '/all-done'
+      router.push(`/me/profile/edit?next=${nextStep}`)
+    }
+  }, [router, userProfileRes])
 
   return {
-    hasAuth: false,
-    userProfile: undefined,
-    isLoading: false,
-    refetchProfile: async () => {},
+    hasAuth,
+    userProfile: userProfileRes?.data,
+    isLoading: isTokenLoading || (hasToken && isLoading),
+    refetchProfile,
   }
 }
